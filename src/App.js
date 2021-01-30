12665d6ee4d28a7d90a09066ebc2b43575d05585
jnemo44 <64214120+jnemo44@users.noreply.github.com>
2021-01-30 14:11:31 -0600

    Finished the assignment

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
-import {Route, NavLink, Switch } from 'react-router-dom';
+import {Route, NavLink, Switch, Redirect } from 'react-router-dom';
 
 import Courses from './containers/Courses/Courses';
 import Users from './containers/Users/Users';
-import Course from './containers/Course/Course';
+//import Course from './containers/Course/Course';
 
 class App extends Component {
   render () {
@@ -33,6 +33,8 @@ class App extends Component {
           <Route path='/courses' component={Courses}/>
           <Route path='/users' component={Users}/>
           {/*<Route path={'/courses/:courseId'} component={Course}/>*/}
+          <Redirect from='/all-courses' to='courses'/>
+          <Route render={() => <h1>404 Not found</h1>}/>
         </Switch>
         
       
@@ -42,9 +44,9 @@ class App extends Component {
           <p><span>&#10003;</span>Make the courses in "Courses" clickable by adding a link and load the "Course" component in the place of "Courses" (without passing any data for now)</p>
           <p><span>&#10003;</span>Pass the course ID to the "Course" page and output it there</p>
           <p><span>&#10003;</span>Pass the course title to the "Course" page - pass it as a param or score bonus points by passing it as query params (you need to manually parse them though!)</p>
-          <li>Load the "Course" component as a nested component of "Courses"</li>
-          <li>Add a 404 error page and render it for any unknown routes</li>
-          <li>Redirect requests to /all-courses to /courses (=> Your "Courses" page)</li>
+          <p><span>&#10003;</span>Load the "Course" component as a nested component of "Courses"</p>
+          <p><span>&#10003;</span>Add a 404 error page and render it for any unknown routes</p>
+          <p><span>&#10003;</span>Redirect requests to /all-courses to /courses (=> Your "Courses" page)</p>
         </ol>
 
         </div>     
